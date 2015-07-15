4ce011c48fcb6b446cc342c91effac891e2d6697
sali <sali2@loyalty.com>
2015-07-15 15:52:33 -0400

    updated labels and clean up

--- src/main/java/com/alphasystem/arabic/model/HiddenNounStatus.java
+++ src/main/java/com/alphasystem/arabic/model/HiddenNounStatus.java
@@ -1,50 +1,52 @@
 /**
- * 
+ *
  */
 package com.alphasystem.arabic.model;
 
 
+import static com.alphasystem.arabic.model.ArabicLetterType.*;
+import static com.alphasystem.arabic.model.ArabicWord.getWord;
+
 /**
  * @author sali
- * 
  */
 public enum HiddenNounStatus implements SarfMemberType {
 
-	NOMINATIVE_SINGULAR(null),
+    NOMINATIVE_SINGULAR(getWord(MEEM, RA, FA, AIN, SPACE, MEEM, FA, RA, DAL)),
 
-	NOMINATIVE_DUAL(null),
+    NOMINATIVE_DUAL(getWord(MEEM, RA, FA, AIN, SPACE, MEEM, THA, NOON, ALIF_MAKSURA)),
 
-	NOMINATIVE_PLURAL(null),
+    NOMINATIVE_PLURAL(getWord(MEEM, RA, FA, AIN, SPACE, JEEM, MEEM, AIN)),
 
-	ACCUSATIVE_SINGULAR(null),
+    ACCUSATIVE_SINGULAR(getWord(MEEM, NOON, SAD, WAW, BA, SPACE, MEEM, FA, RA, DAL)),
 
-	ACCUSATIVE_DUAL(null),
+    ACCUSATIVE_DUAL(getWord(MEEM, NOON, SAD, WAW, BA, SPACE, MEEM, THA, NOON, ALIF_MAKSURA)),
 
-	ACCUSATIVE_PLURAL(null),
+    ACCUSATIVE_PLURAL(getWord(MEEM, NOON, SAD, WAW, BA, SPACE, JEEM, MEEM, AIN)),
 
-	GENITIVE_SINGULAR(null),
+    GENITIVE_SINGULAR(getWord(MEEM, JEEM, RA, WAW, RA, SPACE, MEEM, FA, RA, DAL)),
 
-	GENITIVE_DUAL(null),
+    GENITIVE_DUAL(getWord(MEEM, JEEM, RA, WAW, RA, SPACE, MEEM, THA, NOON, ALIF_MAKSURA)),
 
-	GENITIVE_PLURAL(null);
+    GENITIVE_PLURAL(getWord(MEEM, JEEM, RA, WAW, RA, SPACE, MEEM, THA, NOON, ALIF_MAKSURA));
 
-	private final ArabicWord label;
+    private final ArabicWord label;
 
-	/**
-	 * @param label
-	 */
-	private HiddenNounStatus(ArabicWord label) {
-		this.label = label;
-	}
+    /**
+     * @param label
+     */
+    HiddenNounStatus(ArabicWord label) {
+        this.label = label;
+    }
 
-	@Override
-	public ArabicWord getMemberTermLabel() {
-		return label;
-	}
+    @Override
+    public ArabicWord getMemberTermLabel() {
+        return label;
+    }
 
-	@Override
-	public String getMemberTermName() {
-		return name();
-	}
+    @Override
+    public String getMemberTermName() {
+        return name();
+    }
 
 }
--- src/main/java/com/alphasystem/arabic/model/HiddenPronounStatus.java
+++ src/main/java/com/alphasystem/arabic/model/HiddenPronounStatus.java
@@ -1,60 +1,74 @@
 /**
- * 
+ *
  */
 package com.alphasystem.arabic.model;
 
 
+import static com.alphasystem.arabic.model.ArabicLetterType.*;
+import static com.alphasystem.arabic.model.ArabicWord.getWord;
+
 /**
  * @author sali
- * 
  */
 public enum HiddenPronounStatus implements SarfMemberType {
 
-	THIRD_PERSON_MASCULINE_SINGULAR(null),
+    THIRD_PERSON_MASCULINE_SINGULAR(getWord(GHAIN, ALIF, YA_HAMZA_ABOVE, BA, SPACE, MEEM, THAL, KAF, RA,
+            SPACE, MEEM, FA, RA, DAL)),
 
-	THIRD_PERSON_MASCULINE_DUAL(null),
+    THIRD_PERSON_MASCULINE_DUAL(getWord(GHAIN, ALIF, YA_HAMZA_ABOVE, BA, SPACE, MEEM, THAL, KAF, RA,
+            SPACE, MEEM, THA, NOON, ALIF_MAKSURA)),
 
-	THIRD_PERSON_MASCULINE_PLURAL(null),
+    THIRD_PERSON_MASCULINE_PLURAL(getWord(GHAIN, ALIF, YA_HAMZA_ABOVE, BA, SPACE, MEEM, THAL, KAF, RA,
+            SPACE, JEEM, MEEM, AIN)),
 
-	THIRD_PERSON_FEMININE_SINGULAR(null),
+    THIRD_PERSON_FEMININE_SINGULAR(getWord(GHAIN, ALIF, YA_HAMZA_ABOVE, BA, SPACE, MEEM, WAW_HAMZA_ABOVE, NOON, THA,
+            SPACE, MEEM, FA, RA, DAL)),
 
-	THIRD_PERSON_FEMININE_DUAL(null),
+    THIRD_PERSON_FEMININE_DUAL(getWord(GHAIN, ALIF, YA_HAMZA_ABOVE, BA, SPACE, MEEM, WAW_HAMZA_ABOVE, NOON, THA,
+            SPACE, MEEM, THA, NOON, ALIF_MAKSURA)),
 
-	THIRD_PERSON_FEMININE_PLURAL(null),
+    THIRD_PERSON_FEMININE_PLURAL(getWord(GHAIN, ALIF, YA_HAMZA_ABOVE, BA, SPACE, MEEM, WAW_HAMZA_ABOVE, NOON, THA,
+            SPACE, JEEM, MEEM, AIN)),
 
-	SECOND_PERSON_MASCULINE_SINGULAR(null),
+    SECOND_PERSON_MASCULINE_SINGULAR(getWord(MEEM, KHA, ALIF, TTA, BA, SPACE, MEEM, THAL, KAF, RA,
+            SPACE, MEEM, FA, RA, DAL)),
 
-	SECOND_PERSON_MASCULINE_DUAL(null),
+    SECOND_PERSON_MASCULINE_DUAL(getWord(MEEM, KHA, ALIF, TTA, BA, SPACE, MEEM, THAL, KAF, RA,
+            SPACE, MEEM, THA, NOON, ALIF_MAKSURA)),
 
-	SECOND_PERSON_MASCULINE_PLURAL(null),
+    SECOND_PERSON_MASCULINE_PLURAL(getWord(MEEM, KHA, ALIF, TTA, BA, SPACE, MEEM, THAL, KAF, RA,
+            SPACE, JEEM, MEEM, AIN)),
 
-	SECOND_PERSON_FEMININE_SINGULAR(null),
+    SECOND_PERSON_FEMININE_SINGULAR(getWord(MEEM, KHA, ALIF, TTA, BA, SPACE, MEEM, WAW_HAMZA_ABOVE, NOON, THA,
+            SPACE, MEEM, FA, RA, DAL)),
 
-	SECOND_PERSON_FEMININE_DUAL(null),
+    SECOND_PERSON_FEMININE_DUAL(getWord(MEEM, KHA, ALIF, TTA, BA, SPACE, MEEM, WAW_HAMZA_ABOVE, NOON, THA,
+            SPACE, MEEM, THA, NOON, ALIF_MAKSURA)),
 
-	SECOND_PERSON_FEMININE_PLURAL(null),
+    SECOND_PERSON_FEMININE_PLURAL(getWord(MEEM, KHA, ALIF, TTA, BA, SPACE, MEEM, WAW_HAMZA_ABOVE, NOON, THA,
+            SPACE, JEEM, MEEM, AIN)),
 
-	FIRST_PERSON_SINGULAR(null),
+    FIRST_PERSON_SINGULAR(getWord(MEEM, TA, KHA, LAM, MEEM, SPACE, MEEM, FA, RA, DAL)),
 
-	FIRST_PERSON_PLURAL(null);
+    FIRST_PERSON_PLURAL(getWord(MEEM, TA, KHA, LAM, MEEM, SPACE, MEEM, JEEM, MEEM, AIN));
 
-	private final ArabicWord label;
+    private final ArabicWord label;
 
-	/**
-	 * @param label
-	 */
-	private HiddenPronounStatus(ArabicWord label) {
-		this.label = label;
-	}
+    /**
+     * @param label
+     */
+    HiddenPronounStatus(ArabicWord label) {
+        this.label = label;
+    }
 
-	@Override
-	public ArabicWord getMemberTermLabel() {
-		return label;
-	}
+    @Override
+    public ArabicWord getMemberTermLabel() {
+        return label;
+    }
 
-	@Override
-	public String getMemberTermName() {
-		return name();
-	}
+    @Override
+    public String getMemberTermName() {
+        return name();
+    }
 
 }
