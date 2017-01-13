80b4a0869b91ef5060c7f016f234de0d726e334c
sfali23 <f.syed.ali@gmail.com>
2017-01-13 08:42:40 -0500

    convenience methods to get font

--- src/main/java/com/alphasystem/arabic/ui/util/FontUtilities.java
+++ src/main/java/com/alphasystem/arabic/ui/util/FontUtilities.java
@@ -1,6 +1,8 @@
 package com.alphasystem.arabic.ui.util;
 
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 import java.util.List;
 
@@ -16,6 +18,9 @@ public final class FontUtilities {
     private static final String CANDARA = "Candara";
     public static final long DEFAULT_ARABIC_FONT_SIZE = 20;
     public static final int DEFAULT_ENGLISH_FONT_SIZE = 12;
+
+    public static final Font ENGLISH_FONT_14 = getEnglishFont(14);
+    public static final Font ARABIC_FONT_24 = getArabicFont(24L);
 
     /**
      * Do not let anyone instantiate this class
@@ -43,5 +48,21 @@ public final class FontUtilities {
             return CANDARA;
         }
         return ARIAL;
+    }
+
+    public static Font getArabicFont(long size){
+        return getArabicFont(FontWeight.BLACK, FontPosture.REGULAR, size);
+    }
+
+    public static Font getArabicFont(FontWeight weight, FontPosture posture, long size){
+        return Font.font(getDefaultArabicFontName(), weight, posture, size);
+    }
+
+    public static Font getEnglishFont(long size){
+        return getEnglishFont(FontWeight.BLACK, FontPosture.REGULAR, size);
+    }
+
+    public static Font getEnglishFont(FontWeight weight, FontPosture posture, long size){
+        return Font.font(getDefaultEnglishFont(), weight, posture, size);
     }
 }
